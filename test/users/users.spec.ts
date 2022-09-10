438dade181ebf3705e0e3d7c63364b597c101842
Angelo-Netho <angelonetho@gmail.com>
2022-09-10 17:57:52 -0300

    Definição dos dados do usuário

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -3,8 +3,19 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('User', () => {
-  test.only('it should create an user', async () => {
-    const userPayLoad = { email: 'test@test.com', username: 'test', password: 'test' }
-    await supertest(BASE_URL).post('/users').send(userPayLoad).expect(201)
+  test.only('it should create an user', async (assert) => {
+    const userPayLoad = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      avatar: 'https://images.com/image/1',
+    }
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayLoad).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayLoad.email)
+    assert.equal(body.user.username, userPayLoad.username)
+    assert.equal(body.user.password, userPayLoad.password)
+    assert.equal(body.user.avatar, userPayLoad.avatar)
   })
 })
