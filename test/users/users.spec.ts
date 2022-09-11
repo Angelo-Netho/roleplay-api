95523cd05205bd6be093b102b2c884280764a011
Angelo-Netho <angelonetho@gmail.com>
2022-09-11 19:10:42 -0300

    testando a atualização da senha do usuário

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,3 +1,5 @@
+import Hash from '@ioc:Adonis/Core/Hash'
+import { assert } from '@japa/preset-adonis'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { UserFactory } from 'Database/factories'
 import test from 'japa'
@@ -90,7 +92,7 @@ test.group('User', (group) => {
     assert.equal(body.status, 422)
   })
 
-  test.only('it should update an user', async (assert) => {
+  test('it should update an user', async (assert) => {
     const { id, password } = await UserFactory.create()
     const email = 'test@test.com'
     const avatar = 'https://netho.dev/test.png'
@@ -108,6 +110,26 @@ test.group('User', (group) => {
     assert.equal(body.user.email, email)
     assert.equal(body.user.avatar, avatar)
     assert.equal(body.user.id, id)
+  })
+
+  test('it should update the password of the user', async (assert) => {
+    const user = await UserFactory.create()
+    const password = 'test'
+
+    const { body } = await supertest(BASE_URL)
+      .put(`/users/${user.id}`)
+      .send({
+        email: user.email,
+        avatar: user.avatar,
+        password,
+      })
+      .expect(200)
+
+    assert.exists(body.user, 'User undefined')
+    assert.equal(body.user.id, user.id)
+
+    await user.refresh()
+    assert.isTrue(await Hash.verify(user.password, password))
   })
 
   group.beforeEach(async () => {
