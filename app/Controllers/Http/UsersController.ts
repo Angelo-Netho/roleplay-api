bb36cc299745ebf718e4c185d317d5fe122b09f7
Angelo-Netho <angelonetho@gmail.com>
2022-09-11 17:13:17 -0300

    testando o cadastro de usuário com dados inválidos

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -5,6 +5,10 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayLoad = request.only(['email', 'username', 'password', 'avatar'])
+
+    if (!userPayLoad.email || !userPayLoad.username || !userPayLoad.password)
+      throw new BadRequest('provide required data', 422)
+
     const userByEmail = await User.findBy('email', userPayLoad.email)
     const userByUsername = await User.findBy('username', userPayLoad.username)
 
