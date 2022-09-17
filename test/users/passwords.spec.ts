02f5a4a072cc77ce01ffb389f5baf8480e8f557c
Angelo-Netho <angelonetho@gmail.com>
2022-09-17 15:25:59 -0300

    criando o validator

--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -35,22 +35,26 @@ test.group('Password', (group) => {
     Mail.restore()
   }).timeout(0)
 
-  test
-    .only('it shoud create a reset password token', async (assert) => {
-      const user = await UserFactory.create()
+  test('it shoud create a reset password token', async (assert) => {
+    const user = await UserFactory.create()
 
-      await supertest(BASE_URL)
-        .post('/forgot-password')
-        .send({
-          email: user.email,
-          resetPasswordUrl: 'url',
-        })
-        .expect(204)
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: 'url',
+      })
+      .expect(204)
 
-      const tokens = await user.related('tokens').query()
-      assert.isNotEmpty(tokens)
-    })
-    .timeout(0)
+    const tokens = await user.related('tokens').query()
+    assert.isNotEmpty(tokens)
+  }).timeout(0)
+
+  test.only('it should return 422 when required data is not provided or data is invalid', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/forgot-password').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
 
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
